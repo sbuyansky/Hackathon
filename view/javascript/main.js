9b0b8aa92279aff13883aabd3bbe3e12b6ef9d5e
Allen Liu <zliu224@wisc.edu>
2012-10-20 01:20:02 -0500

    Half way to finish the login page beyond the interest tags

--- view/javascript/main.js
+++ view/javascript/main.js
@@ -1,4 +1,38 @@
 // JavaScript Document
+var gender;
+function createXMLRequest()
+{
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{// code for IE7+, Firefox, Chrome, Opera, Safari
+	  xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{// code for IE6, IE5
+  	xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  	}
+	return xmlhttp;
+}
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push($.trim(this.value) || '');
+        } else {
+            o[this.name] = $.trim(this.value) || '';
+        }
+    });
+	if(gender != null)
+	{
+		o["gender"] = gender;
+	}
+	return o;
+};
 $(document).ready(function(e) {
    	$("#signin").click(function(e) {
 		$("#signup").removeClass("active");
@@ -8,12 +42,20 @@ $(document).ready(function(e) {
 		$("#signin").removeClass("active");
         $(this).addClass("active");
     });
+	$("#signin").mouseleave(function(e){
+			$(this).removeClass("active");
+	});
+	$("#signup").mouseleave(function(e) {
+        $(this).removeClass("active");
+    });
 	$("#male").click(function(e){
 		$(this).addClass("active");
 		if($("#female").hasClass("active"))
 		{
 			$("#female").removeClass("active");	
 		}
+		gender = $(this).val();
+		e.preventDefault();
 	});
 	$("#female").click(function(e){
 		$(this).addClass("active");
@@ -21,14 +63,42 @@ $(document).ready(function(e) {
 		{
 			$("#male").removeClass("active");	
 		}
+		gender = $(this).val();
+		e.preventDefault();
+	});
+	$("#Continue").click(function(e) {
+		if($("#first_name").val() == "" || $("#last_name").val() == "" || $("#foo").val() == "" || $("#password").val() == "" || $("#birthdate").val() == "")
+		{
+			e.preventDefault();
+			alert("Make sure you entered the requried field: last name, first_name, email, password, and birthdate");	
+		}
+		else
+		{
+			
+        	var text = JSON.stringify($("#signUp_form").serializeObject());
+			var url = "signup.php";
+			$.ajax({
+  				type: 'POST',
+  				url: url,
+  				data: text,
+  				success: success,
+  				dataType: json
+			}, function(data){
+				alert("Data Loaded" + data);	
+			});
+		}
 	});
 	$("a#sign2").fancybox({
 		'hideOnContentClick': true,
-		'autoSize': false,
-		'width': 600,
-		'height': 800
+		'closeBtn': false,
+		'beforeClose' : function() {
+        	return window.confirm('Are you sure you want to close?');
+		}
 	});
 	$("a#sign1").fancybox({
 		'hideOnContentClick': true,
+		'beforeClose' : function() {
+        return window.confirm('Are you sure you want to close?');
+		}
 	});
 });
